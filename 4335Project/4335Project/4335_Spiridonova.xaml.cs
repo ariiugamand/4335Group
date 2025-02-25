7144a9b71bd3dfb66b2b370551171dbe64812a65
ksenia <x.spiridonova@mail.ru>
2025-02-25 20:54:58 +0300

    Added changes

---
+++ 4335Project/4335Project/4335_Spiridonova.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _4335Project
+{
+    /// <summary>
+    /// Логика взаимодействия для _4335_Spiridonova.xaml
+    /// </summary>
+    public partial class _4335_Spiridonova : Window
+    {
+        public _4335_Spiridonova()
+        {
+            InitializeComponent();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Spiridonova Ksenia 18 years old");
+        }
+    }
+}
